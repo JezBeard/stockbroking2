830c8258029bc4ab9fdd48d24d3abc3db96b2bc1
JezBeard <jezbeard@gmail.com>
2023-11-24 14:37:00 +0800

    Update app.py

--- app.py
+++ app.py
@@ -18,15 +18,15 @@ pinecone.environment = st.secrets["PINECONE_ENVIRONMENT"]
 
 embed_model = OpenAIEmbeddings(model="text-embedding-ada-002")
 
+# Initialize the Pinecone vector store
+vectorstore = Pinecone(index, embed_model.embed_query, text_field)
+
 index_name = 'stocks7'
 
 text_field = "text"
 
 # Initialize the Pinecone index
 index = pinecone.Index(index_name)
-
-# Initialize the Pinecone vector store
-vectorstore = Pinecone(index, embed_model.embed_query, text_field)
 
 # Initialize the LLM
 llm = ChatOpenAI(
