ec3e4e1e77d72d3dd3f6f5c8775865f8587be81d
JezBeard <jezbeard@gmail.com>
2023-11-24 11:56:59 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,61 @@
+import streamlit as st
+import pinecone
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import Pinecone
+from langchain.chat_models import ChatOpenAI
+from langchain.chains.question_answering import load_qa_chain
+from langchain.chains import RetrievalQA
+from langchain.callbacks import get_openai_callback
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+import openai
+import time
 
+openai.api_key = st.secrets["OPENAI_API_KEY"]
+
+# Set the Pinecone API key
+pinecone.api_key = st.secrets["PINECONE_API_KEY"]
+pinecone.environment = st.secrets["PINECONE_ENVIRONMENT"]
+
+embed_model = OpenAIEmbeddings(model="text-embedding-ada-002")
+
+index_name = 'stocks7'
+
+text_field = "text"
+
+# Initialize the Pinecone index
+index = pinecone.Index(index_name)
+
+# Initialize the Pinecone vector store
+vectorstore = Pinecone(index, embed.embed_query, text_field)
+
+# Initialize the LLM
+llm = ChatOpenAI(
+    openai_api_key=OPENAI_API_KEY,
+    model_name='gpt-3.5-turbo',
+    temperature=0.0
+)
+
+def main():
+    st.header("Chat to a Document 💬👨🏻‍💻🤖")
+
+    # Ask a question about the documents in the index
+    query = st.text_input("Ask question's about your document:")
+
+    suggestions = ["", "What is the main topic of the document?", "Summarize the document in 200 words?", "Provide a bullet point list of the key points mentioned in the document?", "Create the headings and subheadings for Powerpoint slides", "Translate the first paragraph to French"]
+
+    suggestion = st.selectbox("Or select a suggestion: (ENSURE QUESTION FIELD ABOVE IS BLANK)", suggestions, index=0)
+
+    if query or suggestion:
+        if suggestion:
+            query = suggestion
+
+        # Perform the similarity search
+        results = vectorstore.similarity_search(query, k=10)  # Return 10 most relevant docs
+
+        # Use the LLM to generate a response based on the retrieved documents
+        for result in results:
+            response = llm.generate(result)
+            parse_response(response)
+
+if __name__ == '__main__':
+    main()
