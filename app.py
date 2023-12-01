fd454617388b92ae96f7b97e206416dd86f8b85f
JezBeard <jezbeard@gmail.com>
2023-12-01 10:35:36 +0800

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ pinecone.environment = st.secrets["PINECONE_ENVIRONMENT"]
 
 embed_model = OpenAIEmbeddings(model="text-embedding-ada-002")
 
-index_name = 'stocks11'
+index_name = 'stocks3'
 
 text_field = "text"
 
